06637b1d06142fb6a73432a24e96357c3badbc09
Loren Carvalho <lcarvalho@linkedin.com>
2020-01-09 13:42:05 -0600

    Initial commit.

--- README.rst
+++ README.rst
@@ -1,2 +1,16 @@
-# zipfile38
 A backport of the zipfile module from Python 3.8, which contains notable improvements such as the "strict_timestamps" keyword argument (which enables the creation of reproducible zip archives).
+
+installation::
+
+    pip install zipfile38
+
+usage:
+
+.. code:: python
+
+    if sys.version_info >= (3, 8):
+        import zipfile
+    else:
+        import zipfile38 as zipfile
+
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+setup(
+    name='zipfile38',
+    version='0.0.2',
+    description='Read and write ZIP files - backport of the zipfile module from Python 3.8',
+    long_description = readme,
+    author='Loren Carvalho',
+    url='https://github.com/lorencarvalho/zipfile38',
+    py_modules=['zipfile38'],
+    classifiers=[
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Archiving :: Compression',
+    ],
+)
